ae1a11e5e00213a5cd824a622e75c6c86693631f
Matheus Henrique Dos Santos <141289527+tteuwDev@users.noreply.github.com>
2024-02-02 16:21:12 -0300

    calculadora Ex de javaScript

--- src/script.js
+++ src/script.js
@@ -1,7 +1,41 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+function Calculadora(numero1, numero2, operador) {
 //ESCREVA SEU CÓDIGO AQUI!!
+let resultado
+let num1 = parseInt (numero1)
+let num2 = parseInt (numero2)
+
+switch (operador) {
+    case "+":
+      resultado = num1 + num2
+    break;
+    case "-":
+      resultado = num1 - num2
+    break;
+    case "/":
+      resultado = num1 / num2
+    break;
+    case "*":
+      resultado = num1 * num2
+    break
+    case "e":
+      let contador = 1
+      resultado = num1 * num1
+      while (contador < num2 - 1){
+        resultado = resultado * num1 
+        contador++
+      }
+      break;
+  }
+    
+  if (resultado == undefined || resultado > 1000000)
+    resultado = "ERRO"
+
+  return resultado
+}
+  
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
